1bd80bde97bf7c30f8c9043ec169d324beade0a2
AhmadRizal69 <rizalahmad1721@gmai.com>
2022-01-22 16:43:42 +0700

    Pertemuan 9

---
+++ WEBPertemuan9/koneksi.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'wpsmt5';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+$result = mysqli_query($conn, 'SELECT * FROM calon_mhs');
+
+
+
+// Ambil data dari tabel calon
+// mysqli_fetch_row(); mengembalikan data dalam bentuk numerik
+// mysqli_fetch_assoc(); mengembalikan data dalam nama field
+// mysqli_fetch_array(); mengembalikan data dalam bentuk numerik dan nama field
+
+ //while ($camaba = mysqli_fetch_array($result)){
+//var_dump($camaba);
+//}
+?>
